a81d8f868545a67a22f35185ceb9623e1ca893e8
Velenir <Velenir@users.noreply.github.com>
2016-11-22 16:21:44 +0300

    Initial commit

---
+++ libs/parts.js
@@ -0,0 +1,160 @@
+const webpack = require('webpack');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+exports.clean = function(path) {
+	return {
+		plugins: [
+			new CleanWebpackPlugin([path], {
+				// Without `root` CleanWebpackPlugin won't point to our
+				// project and will fail to work.
+				root: process.cwd()
+			})
+		]
+	};
+};
+
+// for development
+exports.devServer = function(options) {
+	return {
+		devServer: {
+			// Enable history API fallback so HTML5 History API based
+			// routing works. This is a good default that will come
+			// in handy in more complicated setups.
+			historyApiFallback: true,
+
+			// Unlike the cli flag, this doesn't set
+			// HotModuleReplacementPlugin!
+			hot: true,
+			inline: true,
+
+			// Display only errors to reduce the amount of output.
+			stats: 'errors-only',
+
+			// Parse host and port from env to allow customization.
+			//
+			// If you use Vagrant or Cloud9, set
+			// host: options.host || '0.0.0.0';
+			//
+			// 0.0.0.0 is available to all network devices
+			// unlike default `localhost`.
+			host: options.host, // Defaults to `localhost`
+			port: options.port // Defaults to 8080
+		},
+		plugins: [
+			// Enable multi-pass compilation for enhanced performance
+			// in larger projects. Good default.
+			new webpack.HotModuleReplacementPlugin({
+				multiStep: true
+			})
+		]
+	};
+};
+
+exports.indexTemplate = function(options) {
+	return {
+		plugins: [
+			new HtmlWebpackPlugin(options)
+		]
+	};
+};
+
+// for development
+exports.setupCSS = function(paths) {
+	return {
+		module: {
+			loaders: [
+				{
+					test: /\.css$/,
+					loaders: ['style', 'css', 'postcss'],
+					include: paths
+				},
+				{
+					test: /\.scss$/,
+					loaders: ['style', 'css', 'postcss', 'sass'],
+					include: paths
+				}
+			]
+		}
+	};
+};
+
+// for production
+exports.extractCSS = function(paths) {
+	return {
+		module: {
+			loaders: [
+				// Extract CSS during build
+				{
+					test: /\.css$/,
+					loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+					include: paths
+				},
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract('style', 'css!postcss!sass'),
+					include: paths
+				}
+			]
+		},
+		plugins: [
+			// Output extracted CSS to a file
+			new ExtractTextPlugin('css/[name].[chunkhash].css')
+		]
+	};
+};
+
+// for production
+exports.minify = function() {
+	return {
+		plugins: [
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			})
+		]
+	};
+};
+
+exports.setFreeVariables = function(definitions) {
+	const env = {};
+	for(let key of Object.keys(definitions)) {
+		env[key] = JSON.stringify(definitions[key]);
+	}
+
+	return {
+		plugins: [
+			new webpack.DefinePlugin(env)
+		]
+	};
+};
+
+// for production
+exports.extractBundle = function(options) {
+	const entry = {};
+	entry[options.name] = options.entries;
+
+	return {
+		// Define an entry point needed for splitting.
+		entry: entry,
+		plugins: [
+			// Extract bundle and manifest files. Manifest is
+			// needed for reliable caching.
+			new webpack.optimize.CommonsChunkPlugin({
+				names: [options.name, 'manifest']
+			})
+		]
+	};
+};
+
+// for production only
+exports.deduplicate = function() {
+	return {
+		plugins: [
+			new webpack.optimize.DedupePlugin()
+		]
+	};
+};
---
+++ webpack.config.js
@@ -0,0 +1,163 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+
+const validate = require('webpack-validator');
+
+const parts = require('./libs/parts');
+
+const stylelint = require('stylelint');
+const autoprefixer = require('autoprefixer');
+
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	style: path.join(__dirname, 'app', 'main.scss'),
+	build: path.join(__dirname, 'build')
+};
+
+
+const common = {
+	module: {
+		// preLoaders are executed before loaders, postLoaders -- after
+		preLoaders: [
+			{
+				test: /\.jsx?$/,
+				loaders: ['eslint'],
+				include: PATHS.app
+			},
+			{
+				test: /\.s?css$/,
+				loaders: ['postcss?pack=linter'],
+				include: PATHS.app
+			}
+		],
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				// Enable caching for improved performance during development
+				// It uses default OS directory by default. If you need
+				// something more custom, pass a path to it.
+				// I.e., babel?cacheDirectory=<path>
+				loaders: ['babel?cacheDirectory'],
+				// Parse only app files! Without this it will go through
+				// the entire project. In addition to being slow,
+				// that will most likely result in an error.
+				include: PATHS.app
+			},
+			{
+				test: /\.svg$/,
+				loaders: [
+					'file?name=img/[name].[hash].[ext]'
+				]
+			}
+		]
+	},
+	postcss: function () {
+		return {
+			linter: [stylelint({
+				configFile: '.stylelintrc'
+			})],
+			defaults: [autoprefixer]
+		};
+	},
+
+	// Entry accepts a path or an object of entries.
+	// We'll be using the latter form given it's
+	// convenient with more complex configurations.
+	entry: {
+		style: PATHS.style,
+		app: PATHS.app
+	},
+	output: {
+		path: PATHS.build,
+		filename: 'js/[name].js'
+	},
+
+	// Important! Do not remove ''. If you do, imports without
+  // an extension won't work anymore!
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+
+	plugins: [
+		// new FaviconsWebpackPlugin("./app/logo.svg")
+	]
+};
+
+
+let config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+case 'build':
+case 'stats':
+	config = merge(
+		common,
+		{
+			devtool: 'source-map',
+			output: {
+				path: PATHS.build,
+				// Tweak this to match your GitHub project name
+				// publicPath: '/template_project/',
+				filename: 'js/[name].[chunkhash].js',
+				// This is used for require.ensure. The setup
+				// will work without but this is useful to set.
+				chunkFilename: '[chunkhash].js'
+			}
+		},
+
+		parts.clean(PATHS.build),
+
+		// NODE_ENV = 'production' allows for many react and uglify optimisations
+		parts.setFreeVariables({
+			'process.env.NODE_ENV': 'production'
+		}),
+
+		parts.extractBundle({
+			name: 'vendor',
+			entries: ['react']
+		}),
+		parts.minify(),
+		parts.indexTemplate({
+			title: 'Worker\'s Journey',
+			template: '!!pug!./app/index.pug',
+			inject: false,
+			mobile: true,
+			appMountId: "app",
+			// inline: 'style',	// inline files from "style" chunk
+			// excludeJSWithCSS: true,	// don't include any chunks with css in scripts (when .js is a byproduct of already extracted .css)
+			excludeJSChunks: ['style']	// don't include specific chunks in scripts (when .js is a byproduct of already extracted .css)
+		}),
+		parts.extractCSS(PATHS.style),
+		parts.deduplicate()
+	);
+	break;
+default:
+	config = merge(
+		common,
+		{
+			devtool: 'eval-source-map'
+		},
+
+		parts.indexTemplate({
+			title: 'Worker\'s Journey',
+			template: '!!pug!./app/index.pug',
+			inject: false,
+			mobile: true,
+			appMountId: "app"
+		}),
+		parts.setupCSS(PATHS.style),
+		parts.devServer({
+			// Customize host/port here if needed
+			host: process.env.HOST,
+			port: process.env.PORT
+		})
+	);
+}
+
+// Run validator in quiet mode to avoid output in stats
+module.exports = validate(config, {
+	quiet: true
+});
