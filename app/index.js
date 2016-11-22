f5e6f025e07d93dcb9a0d92fddbb89c9ec5de5e8
Velenir <Velenir@users.noreply.github.com>
2016-11-22 22:38:06 +0300

    chore(router): Change hashHistory to browserHistory for now
    Doesn't work in production without server support. Consider changing back for deployment.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/App';
 import Home from './components/Home';
 import About from './components/About';
 import Info from './components/Info';
 
+// TODO maybe change browserHistory to hashHitory when deploying
+
 render((
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Home}/>
 			<Route path="info" component={Info}/>
