29792e24fafb3a2542c5f9418f5222416acc5145
Velenir <Velenir@users.noreply.github.com>
2016-11-28 16:37:31 +0300

    refactor(routing): Group /dedicated_worker/* routes together

--- app/index.js
+++ app/index.js
@@ -14,6 +14,8 @@ if(typeof process !== "undefined" && process.env.NODE_ENV === "production") {
 import App from './components/App';
 import Home from './components/Home';
 import WorkerTypes from './components/WorkerTypes';
+import Dedicated from './components/Dedicated';
+import DedicatedExample from './components/DedicatedExample';
 import About from './components/About';
 import Info from './components/Info';
 
@@ -24,6 +26,10 @@ render((
 		<Route path="/" component={App}>
 			<IndexRoute component={Home}/>
 			<Route path="worker_types" component={WorkerTypes}/>
+			<Route path="dedicated_worker">
+				<IndexRoute component={Dedicated}/>
+				<Route path="example" component={DedicatedExample}/>
+			</Route>
 			<Route path="info" component={Info}/>
 			<Route path="about" component={About}/>
 		</Route>
