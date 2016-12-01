216ae20c42731e2559830a8d0ab1490a15546da5
Velenir <Velenir@users.noreply.github.com>
2016-12-01 17:58:36 +0300

    feat(not-found-route): Add go back link

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-
-// TODO: add Go back link with history.back()
 const NotFound = () => (
 	<div className="app__content__main not-found">
 		<h1 className="not-found__disclaimer">Nothing to see here</h1>
+		Go <a href="" onClick={(e) => (e.preventDefault(), history.back())}>back</a>
 		Go <Link to="/">home</Link>
 	</div>
 );
