1cb821bda5d0abc092b25b3f0ea312ab9b18de37
Velenir <Velenir@users.noreply.github.com>
2016-12-12 22:21:17 +0300

    feat(about): Scaffold About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router';
+import Marked from './Marked';
+
+import aboutMark from '../marked/About/About.md';
+
 
 const About = () => (
 	<div className="app__content__main about">
-		<h2 className="about__title">About</h2>
-		<p className="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Marked mark={aboutMark}/>
+		<div className="steps-navigation">
+			<Link to="resources" className="arrow" title="Resources"/>
+		</div>
 	</div>
 );
 
